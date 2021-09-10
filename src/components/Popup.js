027799b4fe5c30d8acd1c82f207b3b1cc7881acf
Kaif Rabi <30978437+KaifRabi@users.noreply.github.com>
2021-09-10 17:16:19 +0500

    Removed a console.log

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function Popup(props) {
   return (
     <>
-      {console.log(props.update)}
       <div className={props.update === 1 ? "popup" : ""}>{props.func}</div>
     </>
   );
